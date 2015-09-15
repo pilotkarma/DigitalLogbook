010dda8ad8ab3087096ede115978f03031c9f3d1
vanjikumaran <vanjikumaran@gmail.com>
2015-09-15 14:52:30 -0700

    added login logic

---
+++ app/src/main/java/com/pilotkarma/pilot/digitallogbook/activity/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.pilotkarma.pilot.digitallogbook.activity;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.pilotkarma.pilot.digitallogbook.R;
+
+/**
+ * Created by vsivajothy on 9/15/15.
+ */
+public class LoginActivity extends Activity {
+    Button b1,b2;
+    EditText ed1,ed2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+        b1=(Button)findViewById(R.id.button);
+        ed1=(EditText)findViewById(R.id.editText);
+        ed2=(EditText)findViewById(R.id.editText2);
+
+        b2=(Button)findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().equals("admin") &&
+                        ed2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
